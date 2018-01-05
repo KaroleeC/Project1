0ce500388efd10189fcec70a0cc6d107f52a87d1
KaroleeC <kcarlson957@gmail.com>
2018-01-04 18:58:30 -0800

    Added component files

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 
+
+//create redux storage and pass reducers
+const store = createStore();
 
 render(<h1>Food</h1>, document.getElementById('app') );
