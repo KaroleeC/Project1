cc0abbd899785b600fdc2551ceb69388fe93e404
KaroleeC <kcarlson957@gmail.com>
2018-01-06 15:26:27 -0800

    [userpage/reviews] Added frontend user pages and reviews page. Need to connect to database and add styling.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,14 +2,10 @@ const  User = require('../../db/models/users');
 
 const UserController = {
     CreateUser: (req, res) => {
-<<<<<<< HEAD
-        User.create(User.create({
+        User.create({
             username: 'kari',
             password: 'password'
-        }))
-=======
-        User.create(User.create({}))
->>>>>>> commit before merge
+        })
     }
 }
 
