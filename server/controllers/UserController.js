c02b640af2224043bd5469bf287ef2804465328e
KaroleeC <kcarlson957@gmail.com>
2018-01-06 17:33:32 -0800

    commit before merge

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,10 +2,14 @@ const  User = require('../../db/models/users');
 
 const UserController = {
     CreateUser: (req, res) => {
+<<<<<<< HEAD
         User.create(User.create({
             username: 'kari',
             password: 'password'
         }))
+=======
+        User.create(User.create({}))
+>>>>>>> commit before merge
     }
 }
 
