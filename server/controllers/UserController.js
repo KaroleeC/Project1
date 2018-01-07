86f52877f1ea42e20c047a1fc30f22373c8d0c52
KaroleeC <kcarlson957@gmail.com>
2018-01-06 17:12:35 -0800

    [userpage] small change for usercontroller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,11 +1,12 @@
-
+const  User = require('../../db/models/users');
 
 const UserController = {
     CreateUser: (req, res) => {
-        User.create({
+        User.create(User.create({
             username: 'kari',
             password: 'password'
-        })
+        }))
     }
 }
 
+module.exports = UserController;
