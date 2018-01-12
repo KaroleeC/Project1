9ad432293c4e3da5a9514206364585530c49e3b9
qiang song <qiangsong889@yahoo.com>
2018-01-11 18:15:15 -0800

    --

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,9 +2,13 @@ const  User = require('../../db/models/users');
 
 const UserController = {
     CreateUser: (req, res) => {
+<<<<<<< HEAD
         User.create({username: req.username,
         password: req.password 
         });
+=======
+        User.create(User.create({}))
+>>>>>>> --
     }
 }
 
