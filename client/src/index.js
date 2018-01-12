c4104c072a6b7472bd20f9cef8d9d66c0d96148a
Daniel Sanchez <atlow.archive@gmail.com>
2018-01-11 18:17:29 -0800

    [authentication] removed keys

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, BrowserHistory, Link } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'; //makes store available to all containers
 import App from './components/app';
+import Login from './components/login'
 //import all reducers from reducers index
 import allreducers from './reducers/index';
+
 
 
 //create redux storage and pass reducers in createStore
 //
 const store = createStore(allreducers);
 
+class Root extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exactly pattern="/" component={App} />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+  
+
 render(
-<Provider store={ store }>
-<App />
-</Provider>
-, document.getElementById('app') );
+  <Provider store={ store }>
+  <Root />
+  </Provider>
+  , document.getElementById('app')
+);
  
--- config.js
+++ config.js
@@ -1,6 +1,10 @@
-
-
 module.exports = {
     Authorization: "Bearer bcOLcwULgf2GSZj-kFbPxaiEKMM0sLowpy_x3fmEmQyleXj1XI4OsQnnkd-98u886mPFZOgVU_PK9zbHsiiJqDFDnSqCw-jM9Nu3OxBnSaY2P2NVoEXt_D1CmudOWnYx",
-    secret: "EWkAuEI6EpKSWVuMvlv9HQXfxTZkmyp9RXyza79icMDux1JlsMPl243uqD2i0zQP"
+    secret: "EWkAuEI6EpKSWVuMvlv9HQXfxTZkmyp9RXyza79icMDux1JlsMPl243uqD2i0zQP",
+    FIREBASE_apiKey : "AIzaSyB9Qosk7FLBW2QgVGe0BH3OnTD3i9eywpA",
+    FIREBASE_authDomain : "food-thoughts.firebaseapp.com",
+    FIREBASE_dbURL : "https://food-thoughts.firebaseio.com",
+    FIREBASE_projectID : "food-thoughts",
+    FIREBASE_storeBucket : "food-thoughts.appspot.com",
+    FIREBASE_messageID : "132265688440"
 }
