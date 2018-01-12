4b8014b4d22f28a7d2690dfa66162bdf2ce7933d
Daniel Sanchez <atlow.archive@gmail.com>
2018-01-11 18:35:57 -0800

    [fix] firebase fix again

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,22 +2,12 @@ import firebase from 'firebase';
 import keys from '../../config.js'
 
 const config = {
-<<<<<<< HEAD
   apiKey: keys.FIREBASE_apiKey,
   authDomain: keys.FIREBASE_authDomain,
   databaseURL: keys.FIREBASE_dbURL,
   projectId: keys.FIREBASE_projectID,
   storageBucket: keys.FIREBASE_storeBucket,
   messagingSenderId: keys.FIREBASE_messageID
-||||||| merged common ancestors
-  apiKey: "AIzaSyAOURzDm-6wZMBjITw8anb38zNddT9UZ0E",
-  authDomain: "foodthoughts-28044.firebaseapp.com",
-  databaseURL: "https://foodthoughts-28044.firebaseio.com",
-  projectId: "foodthoughts-28044",
-  storageBucket: "foodthoughts-28044.appspot.com",
-  messagingSenderId: "1022593351688"
-=======
->>>>>>> f274fcbefb4bf665fa97cf6a8c171da55c0ab5c2
 };
 
 firebase.initializeApp(config);
