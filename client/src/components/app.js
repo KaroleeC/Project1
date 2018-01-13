c976ed42cd92b3c510272c4f99f5821078bfdad5
qiang song <qiangsong889@yahoo.com>
2018-01-12 20:18:50 -0800

    test

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -22,18 +22,10 @@ class App extends React.Component {
           //set state with data
           this.props.initReviews(res.data);
           console.log('Reviews?', this.props.reviews)
-<<<<<<< HEAD
-        })
-        .catch(err => { console.log('axois get request err (userdisplay.js', err); } );
-        console.log('inside of app componentwillmount here is the user and err', user, err)
-        this.props.activeUser(user);
-      } else {xs
-=======
       })
       .catch(err => { console.log('axois get request err (userdisplay.js', err); } );
         this.props.activeUser(user);// here is a problem
       } else {
->>>>>>> master
         return (
           <div>
             <Login />
