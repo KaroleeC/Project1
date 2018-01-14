70ec6c3ac89635355cd905d0d9557c256de1e91b
Karolee <kcarlson957@gmail.com>
2018-01-13 17:57:06 -0800

    [deploy] changed port syntax

--- server/index.js
+++ server/index.js
@@ -11,6 +11,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 app.use('/api/', routes);
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log('server now is listening on PORT 3000')
 })
