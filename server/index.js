6a2de885df98b5fb5e1f99f7650a41c0c674caba
Karolee <kcarlson957@gmail.com>
2018-01-13 17:57:06 -0800

    [deploy] changed port syntax

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const routes = require('./routes');
 const path = require('path');
 const db = require('../db/models/sync');
 
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 app.use('/api/', routes);
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log('server now is listening on PORT 3000')
 })
