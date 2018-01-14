7e8dec3cd2562e3aafff25d3922190b291cc987d
Daniel Sanchez <atlow.archive@gmail.com>
2018-01-13 16:55:41 -0800

    [styling] minor login styling changes

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -12,13 +12,18 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className="card offset-sm-4 col-sm-4 offset-sm-4" >
-        <h2 className="card-header text-center">Login</h2>
-        <form className="card-body container" >
-          <button onClick={base.googleLogin} className="btn btn-danger col-sm-12" >Login with Google</button>
-          <button onClick={base.facebookLogin} className="btn btn-primary col-sm-12" >Login with Facebook</button>
-          <button onClick={base.githubLogin} className="btn btn-secondary col-sm-12" >Login with GitHub</button>
-        </form>
+      <div>
+        <div className="text-center" style={{marginTop: '40px', fontSize: '100px'}}>
+          <h1>Food Thoughts</h1>
+        </div>
+        <div className="card offset-sm-4 col-sm-4 offset-sm-4 border-secondary" style={{marginTop: '50px'}} >
+          <h2 className="card-header text-center">Login</h2>
+          <form className="card-body container">
+            <button onClick={base.googleLogin} className="btn btn-danger col-sm-12" style={{marginBottom: '1%'}}>Google</button>
+            <button onClick={base.facebookLogin} className="btn btn-primary col-sm-12" style={{marginBottom: '1%'}}>Facebook</button>
+            <button onClick={base.githubLogin} className="btn btn-secondary col-sm-12">GitHub</button>
+          </form>
+        </div>
       </div>
     )
   }
