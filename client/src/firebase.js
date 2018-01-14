c9ef21d5c05dcd7d058c119c5b35d65c375d0829
Karolee <kcarlson957@gmail.com>
2018-01-13 19:25:37 -0800

    process .env added

---
+++ client/src/firebase.js
@@ -0,0 +1,72 @@
+import firebase from 'firebase';
+import keys from '../../config.js'
+
+
+const config = {
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomain,
+  databaseURL: process.env.FIREBASE_dbURL,
+  projectId: process.env.FIREBASE_projectID,
+  storageBucket: process.env.FIREBASE_storeBucket,
+  messagingSenderId: process.env.FIREBASE_messageID
+};
+
+firebase.initializeApp(config);
+
+let auth = firebase.auth();
+
+const base = {
+  googleLogin: (event) => {
+    event.preventDefault();
+    let provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+      .then((response) => {
+        console.log('This is the response: ', response);
+      })
+      .catch((err) => {
+        console.log('This is the error: ', err);
+      });
+  },
+  facebookLogin: (event) => {
+    event.preventDefault();
+    let provider = new firebase.auth.FacebookAuthProvider();
+    auth.signInWithPopup(provider)
+      .then((response) => {
+        console.log('This is the response: ', response);
+      })
+      .catch((err) => {
+        console.log('This is the error: ', err);
+      });
+  },
+  githubLogin: (event) => {
+    event.preventDefault();
+    let provider = new firebase.auth.GithubAuthProvider();
+    auth.signInWithPopup(provider)
+      .then((response) => {
+        console.log('This is the response: ', response);
+      })
+      .catch((err) => {
+        console.log('This is the error: ', err);
+      });
+  },
+  logout: () => {
+    auth.signOut();
+  },
+  auth: auth,
+  // session: (cb)=> {
+  //   console.log('session!!!!!!!!!')
+  //   auth.onAuthStateChanged(function(user) {
+  //     if (user) {
+  //       // User is signed in.
+  //       console.log(user,cb)
+  //       cb(user)
+  //     } else {
+  //       // No user is signed in.
+  //       cb(null)
+  //     }
+  //   })
+  // }
+}
+
+
+export default base;
