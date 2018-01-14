b0efb79d8e2c826ed3e6b367f192280fe0cb3fdd
Karolee <kcarlson957@gmail.com>
2018-01-13 18:07:09 -0800

    port

--- server/index.js
+++ server/index.js
@@ -11,6 +11,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 app.use('/api/', routes);
 
-app.listen(PORT, function(){
+app.listen(3000, function(){
     console.log('server now is listening on PORT 3000')
 })
