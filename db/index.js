9e2fa6468dacb4c7b73509744367e1dc6348017c
Karolee <kcarlson957@gmail.com>
2018-01-14 18:21:17 -0800

    [db] set cleardb values in db file

--- db/index.js
+++ db/index.js
@@ -1,9 +1,16 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('FoodThoughts', 'root', '', {
-  host: 'localhost',
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'bded3c0e6d36bd',
+  password: '0428dc2c',
+  database: 'heroku_919f88ddaa85c2b',
   port: 3306,
   dialect: 'mysql',
 });
+//mysql://bded3c0e6d36bd:0428dc2c@us-cdbr-iron-east-05.cleardb.net/heroku_919f88ddaa85c2b?reconnect=true
+// host: 'localhost',
+// port: 3306,
+// dialect: 'mysql',
 
 db.authenticate()
   .then(() => {
